bac3081c8f4f7651798951cee24b2f2ce9cc5bff
sowinskipatryk <sowinski.patryk@wp.pl>
2021-11-11 14:52:56 +0100

    better table editing

--- counter.py
+++ counter.py
@@ -4,15 +4,21 @@ file = open('job_offers.txt','r')
 
 x = file.read()
 
-y = x.strip().replace('\n',' ').replace(' - ',' ').replace('  ',' ').replace('(','').replace(')','')
+y = x.strip().replace('\n',' ').replace(' - ',' ').replace('  ',' ').replace('(','').replace(')','').replace(',','').replace(';','').upper()
 
 z = y.split()
 
-a = []
+arr = []
 
 my_dict = {i:z.count(i) for i in z}
 
-a.append(my_dict)
+for i, (k, v) in enumerate(my_dict.items()):
+    arr.append((i+1, k, v))
 
-df = pd.DataFrame(a)
+df = pd.DataFrame(arr)
+df.columns = ['Index','Phrase','Appearances']
+df = df[df['Phrase'].str.contains('HTTP') == False]
+df = df.loc[:, df.columns != 'Index']
+df = df.sort_values('Appearances', ascending=False)
+
 df.to_excel('phrases.xlsx', index=False)
