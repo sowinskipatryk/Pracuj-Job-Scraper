489397443575de13587866d6a30e1275c10ef0c8
sowinskipatryk <sowinski.patryk@wp.pl>
2021-11-16 17:16:15 +0100

    added line to close the browser after scraping data so the computer doesn't run out of memory

--- save_to_xlsx.py
+++ save_to_xlsx.py
@@ -18,7 +18,9 @@ while next_page:
 
         print(f'Scraping page no.{page}')
 
-        url = f'https://www.pracuj.pl/praca/gdansk;wp?rd=10&et=3&pn={page}'
+        url = f'https://www.pracuj.pl/praca/test;kw/it%20-%20rozw%C3%B3j%20oprogramowania;cc,5016/praca%20zdalna;wm,home-office?pn={page}'
+
+        print(url)
 
         browser = start_chrome(url, headless=True)
 
@@ -26,40 +28,48 @@ while next_page:
 
         soup = BeautifulSoup(browser.page_source, 'html.parser')
 
+        browser.close()
+
         result_html = soup.find('div', {'class': 'results'}, id="results")
 
         jobs = result_html.find_all('div', {'class': 'offer__info'})
 
         for job in jobs:
-            job_title = job.find('a', {'class': 'offer-details__title-link'}).text
-            job_link = job.find('a', {'class': 'offer-details__title-link'})['href']
+            try:
+                job_title = job.find('a', {'class': 'offer-details__title-link'}).text
+            except:
+                job_title = ''
+            try:
+                job_link = job.find('a', {'class': 'offer-details__title-link'})['href']
+            except:
+                job_link = ''
             try:
                 job_company = job.find('a', {'class': 'offer-company__name'}).text
-            except AttributeError:
+            except:
                 job_company = ''
             try:
                 job_location = job.find('li', {'class': 'offer-labels__item--location'}).text
-            except AttributeError:
+            except:
                 job_location = ''
             try:
                 job_type = job.find('li', {'data-test': 'list-item-offer-work-modes'}).text
-            except AttributeError:
+            except:
                 job_type = ''
             try:
                 job_level = job.find('li', {'data-test': 'list-item-offer-employment-level'}).text
-            except AttributeError:
+            except:
                 job_level = ''
             try:
                 job_time = job.find('li', {'data-test': 'list-item-offer-work-schedule'}).text
-            except AttributeError:
+            except:
                 job_time = ''
             try:
                 job_salary = job.find('li', {'data-test': 'list-item-offer-salary'}).text
-            except AttributeError:
+            except:
                 job_salary = ''
             try:
                 job_contract = job.find('li', {'data-test': 'list-item-offer-type-of-contract'}).text
-            except AttributeError:
+            except:
                 job_contract = ''
 
             job_titles.append(job_title)
@@ -71,13 +81,14 @@ while next_page:
             job_times.append(job_time)
             job_contracts.append(job_contract)
 
+            print([job_company, job_title, job_location, job_level, job_contract, job_type, job_time, job_link])
         try:
             next_page = soup.find('i', {'class': 'mdi mdi-chevron-right pagination_element-icon'})
 
         except AttributeError:
             next_page = False
 
-        print(f'Page no.{page} scraped')
+        print(f'Page no.{page} scraped\n')
 
         page += 1
 
@@ -85,5 +96,5 @@ while next_page:
 
 df = pd.DataFrame([job_companies, job_titles, job_locations, job_levels, job_contracts, job_types, job_times, job_links]).T
 df.columns = ['Pracodawca', 'Stanowisko','Lokalizacja','Poziom stanowiska','Rodzaj umowy', 'Typ pracy','Wymiar pracy', 'Link']
-df.to_excel('job_offers_pracuj.xlsx', index=False)
+df.to_excel('pracuj_test_zdalnie.xlsx', index=False)
 print('Job offers exported to Excel successfully.')
